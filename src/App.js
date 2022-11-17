110602aa1ff413fc0b2b3fecc1ab4769ecfff1a9
thbob2 <33073278+thbob2@users.noreply.github.com>
2022-11-17 21:30:48 +0100

    implement Pokegame component plus fixed some minor error
    On branch pokegame
     Changes to be committed:
    	modified:   src/App.css
    	modified:   src/App.js
    	modified:   src/components/Pokecard/index.js
    	modified:   src/components/Pokedex/index.js
    	modified:   src/components/Pokegame/index.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
+import Pokegame from './components/Pokegame';
+
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
-			<img
-				src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png"
-				alt=""
-			/>
-			<img
-				src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png"
-				alt=""
-			/>
-			<img
-				src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png"
-				alt=""
-			/>
-			<img
-				src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-				alt=""
-			/>
-			<img
-				src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"
-				alt=""
-			/>
-			<img
-				src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
-				alt=""
-			/>
+			<Pokegame />
 		</div>
 	);
 }
